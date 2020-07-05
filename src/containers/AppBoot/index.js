8d61988bda4d345036302476d48ca6164975dc73
ManviSharma09 <manvis@geekyants.com>
2020-07-05 16:56:20 +0530

    initial commit

---
+++ src/containers/AppBoot/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const App = () => {
+  return <Text>This is default React Native BoilerPlate using recoil</Text>;
+};
+export default App;
